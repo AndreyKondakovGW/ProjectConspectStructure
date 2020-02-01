ea6107f0d50953657437f015b4e76bac789e831a
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2020-02-01 17:25:01 +0300

    Pepped repo again

--- app/UserDBAPI.py
+++ app/UserDBAPI.py
@@ -1,20 +1,18 @@
 from app import db
 from app.models import User
 
-def add_to_db(key,value):
-    db[key] = User(key,value)
+
+def add_to_db(key, value):
+    db[key] = User(key, value)
 
 
-def right_user_data(key,value):
+def right_user_data(key, value):
     if db:
         return db[key].password == int(value)
-    else: return False
+    else:
+        return False
 
 
 def show_all_user():
     if db:
         print(db)
-
-
-
-

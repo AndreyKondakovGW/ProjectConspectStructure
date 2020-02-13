9fddd48cba6e5f8f5d76f2f3d10d012383a3f095
caisilus <serangor@gmail.com>
2020-02-13 15:00:09 +0300

    пересоздал базу данных, чтобы работало свойство unique для поля UserDB.name

--- app/models.py
+++ app/models.py
@@ -67,8 +67,8 @@ Base.metadata.create_all(engine)
 
 
 
-session.add(UserDB('admin', str(123)))
-session.commit()
+#session.add(UserDB('admin', str(123)))
+#session.commit()
 # print(sessoion.query(UserDB).first())
 # print(sessoion.query(UserDB).filter_by(name='admin').first())
 # print(sessoion.query(UserDB).filter_by(name='admin1').first())

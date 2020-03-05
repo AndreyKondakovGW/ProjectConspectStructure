a100fa932ddbc2076e41e74ebf95e8e4c8db6471
caisilus <serangor@gmail.com>
2020-03-05 17:02:06 +0300

    описал основные классы таблиц базы данных

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from app import Session
 from app.__init__ import engine
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String, Date, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from app import db, login
 from flask_login import UserMixin
@@ -64,7 +64,96 @@ class UserDB(UserMixin, Base):
 Base.metadata.create_all(engine)
 
 
+class ConspectsDB(Base):
+    __tablename__ = "Conspects"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    date = Column(Date)
+    name = Column(String)
 
+    def __init__(self, date, name):
+        self.name = name
+        self.date = date
+
+
+class Tag(Base):
+    __tablename__ = "Tags"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, unique=True, nullable=False)
+    user_id = Column(String, nullable=False)
+
+    def __init__(self, name, user_id):
+        self.name = name
+        self.user_id = user_id
+
+
+class ConspectTagRelation(Base):
+    __tablename__ = "ConpectTagRelations"
+    conspect_id = Column(Integer,nullable=False)
+    tag_id = Column(Integer, nullable=False)
+
+    def __init__(self, conspect_id, tag_id):
+        self.conspect_id = conspect_id
+        self.tag_id = tag_id
+
+
+class PhotoDB(Base):
+    __tablename__ = 'Photoes'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    filename = Column(String, unique=True, nullable=False)
+    id_pred = Column(Integer)
+    id_next = Column(Integer)
+    id_conspect = Column(Integer)
+
+    def __init__(self, filename, predname, nextname, conspect_id):
+        self.filename = filename
+        self.id_pred = session.query(PhotoDB.id).filter_by(filename=predname)
+        self.id_next = session.query(PhotoDB.id).filter_by(filename=nextname)
+        self.id_conspect = conspect_id
+
+
+class FragmentDB(Base):
+    __tablename__ = "Fragments"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    photo_id = Column(Integer, nullable=False)
+    x1 = Column(Integer, nullable=False)
+    y1 = Column(Integer, nullable=False)
+    x2 = Column(Integer, nullable=False)
+    y2 = Column(Integer, nullable=False)
+    name = Column(String)
+
+    def __init__(self, photo_id,*_, x1, y1, x2, y2):
+        self.photo_id = photo_id
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+
+    def set_name(self, name):
+        self.name = name
+
+
+class FragmentsRelation(Base):
+    __tablename__ = "FragmentsRelations"
+    id_master = Column(Integer, nullable=False)
+    id_slave = Column(Integer, nullable=False)
+
+    def __init__(self, id_master, id_slave):
+        self.id_master = id_master
+        self.id_slave = id_slave
+
+
+class FragmentToTagRelations(Base):
+    __tablename__ = "FragmentToTagRelations"
+    fragment_id = Column(Integer, nullable=False)
+    tag_id = Column(Integer, nullable=False)
+
+    def __init__(self, fragment_id, tag_id):
+        if (not session.query(FragmentDB).filter_by(id=fragment_id).all()) or (not session.query(Tag).filter_by(id=tag_id).all()):
+            print("Ошибка при создании отношения: не существует искомый тэг или искомый фрагмент")
+            return
+        else:
+            self.fragment_id = fragment_id
+            self.tag_id = tag_id
 
 
 # session.add(UserDB('admin', str(123)))

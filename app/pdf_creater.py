c1a883fb2ed4ba1bbd839beaf347621eff606edf
Andrew <andreykondakov2302@gmail.com>
2020-04-09 19:11:58 +0300

    add better pdf viewer

--- app/pdf_creater.py
+++ app/pdf_creater.py
@@ -2,6 +2,13 @@ from fpdf import FPDF
 import os
 from PIL import Image
 
+class MyPDF(FPDF):
+    def footer(self):
+        self.set_y(-15)
+        self.set_text_color(0, 0, 0)
+        self.set_font('Arial', 'I', 8)
+        self.cell(0, 10, 'Page ' + str(self.page_no()), 0, 0, 'C')
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -16,16 +23,22 @@ def create_simple_pdf(name,text):
 
 def create_pdf_from_images(name, image_list):
     print("creating pdf")
-    pdf = FPDF(orientation='P', unit='mm', format='A4')
+    pdf = MyPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
+    pdf.set_font('Arial','B', 30)
+    pdf.cell(80)
+    pdf.set_text_color(0, 0, 0)
+    pdf.cell(30, 10, name, 0, 10, 'C')
+    pdf.ln(20)
     for img in image_list:
         print(img)
         if os.path.exists(img):
             print("exists")
-            pdf.image(img, w=200)
             pdf.set_font("Arial", size=12)
-            pdf.ln(10)
-            pdf.cell(200, 10, txt="{}".format(img[img.rindex('/')+1:]), ln=1)
+            pdf.cell(25)
+            pdf.image(img, w=150)
+            pdf.ln(5)
+            pdf.cell(200, 10, ln=1)
     print(basedir+'/static/Photo/'+name+'.pdf')
     pdf.output(basedir+'/static/Photo/'+name+'.pdf', dest='F')
 
@@ -33,9 +46,10 @@ def create_pdf_from_images(name, image_list):
 def cut(image_path, left, top, right, bottom, name):
     im = Image.open(basedir+'/static/Photo/'+image_path)
     w, h = im.size
-    if ((left) and (top) and (right) and (bottom)):
-        im1 = im.crop((w*left, h*top, w*right, h*bottom))
-    else:
-        im1=im
+    # if ((left) and (top) and (right) and (bottom)):
+    print(left,top,right,bottom)
+    im1 = im.crop((w*left, h*top, w*right, h*bottom))
+    # else:
+        # im1=im
     im1.save(name)
     #im1.show()
